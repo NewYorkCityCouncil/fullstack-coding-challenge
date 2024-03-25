f8fa43945a7960eade6a3d05ecf668ae0943ab6a
JC-NYCC-1227 <jchei@council.nyc.gov>
2024-03-25 17:42:07 -0400

    update to endpoint urls

--- testApp/complaint_app/urls.py
+++ testApp/complaint_app/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import ComplaintViewSet, OpenCasesViewSet, ClosedCasesViewSet, TopComplaintTypeViewSet
 
 router = routers.SimpleRouter()
-router.register(r'', ComplaintViewSet, basename='complaint')
+router.register(r'allComplaints', ComplaintViewSet, basename='complaint')
 router.register(r'openCases', OpenCasesViewSet, basename='openCases')
 router.register(r'closedCases', ClosedCasesViewSet, basename='closedCases')
 router.register(r'topComplaints', TopComplaintTypeViewSet, basename='topComplaints')
